06087f5318af13eb10829f0ff7bf2e7cad60b1ed
Michał Leszczyński <ml@icedev.pl>
2025-05-26 03:15:48 +0200

    Bump to 0.0.23 and fix dataStructDecoder pk9 attest parsing

--- packages/libburner/src.ts/burnerTagData/dataStructDecoder.ts
+++ packages/libburner/src.ts/burnerTagData/dataStructDecoder.ts
@@ -43,18 +43,24 @@ export async function dataStructDecoder(response: IHaloDataStruct): Promise<IDat
   const pk9RawCompressed = response.data['compressedPublicKey:9']
   const pk2RawCompressed = response.data['compressedPublicKey:2']
   const pk8Attest = response.data['publicKeyAttest:8']
+  const pk9Attest = response.data['publicKeyAttest:9']
   const graffitiRaw = response.data['graffiti:1']
   const latch2 = response.data['latchValue:2']
   let theme = defaultTheme
 
   // Make sure we have everything we need
-  if (
-    response.isPartial ||
-    (typeof pk8RawCompressed !== 'string' && typeof pk9RawCompressed !== 'string') ||
-    typeof pk2RawCompressed !== 'string' ||
-    typeof pk8Attest !== 'string'
-  ) {
-    throw new Error('Missing required parameters.')
+  if (typeof pk2RawCompressed !== 'string') {
+    throw new Error('Missing required pk2 parameters.')
+  }
+
+  if (typeof pk9RawCompressed === 'string') {
+    // pk9 tag case
+    if (typeof pk9Attest !== 'string') {
+      throw new Error('Missing required pk9 parameters.')
+    }
+  } else if (typeof pk8RawCompressed !== 'string' || typeof pk8Attest !== 'string') {
+    // pk8 tag case
+    throw new Error('Missing required pk8 parameters.')
   }
 
   let _latchDecoded = null
@@ -76,10 +82,14 @@ export async function dataStructDecoder(response: IHaloDataStruct): Promise<IDat
     const smartAccount = await computeGiftcardAddress(eoaAddress)
     const btcAddress = pknToAddressBTC(pk9RawCompressed)
 
+    if (typeof pk9Attest !== 'string') {
+      throw new Error('Missing pk9Attest.')
+    }
+
     return {
       pk2: uncompressPk(pk2RawCompressed),
       pkN: uncompressPk(pk9RawCompressed),
-      pkNAttest: undefined,
+      pkNAttest: pk9Attest,
       eoaAddress,
       smartAccount,
       btcAddress,
@@ -97,6 +107,10 @@ export async function dataStructDecoder(response: IHaloDataStruct): Promise<IDat
     const eoaAddress = pknToAddressETH(pk8RawCompressed)
     const smartAccount = await computeGiftcardAddress(eoaAddress)
     const btcAddress = pknToAddressBTC(pk8RawCompressed)
+
+    if (typeof pk8Attest !== 'string') {
+      throw new Error('Missing pk8Attest.')
+    }
 
     return {
       pk2: uncompressPk(pk2RawCompressed),
@@ -125,10 +139,14 @@ export async function dataStructDecoder(response: IHaloDataStruct): Promise<IDat
     const smartAccount = await computeGiftcardAddress(eoaAddress)
     const btcAddress = pknToAddressBTC(pk9RawCompressed)
 
+    if (typeof pk9Attest !== 'string') {
+      throw new Error('Missing pk9Attest.')
+    }
+
     return {
       pk2: uncompressPk(pk2RawCompressed),
       pkN: uncompressPk(pk9RawCompressed),
-      pkNAttest: undefined,
+      pkNAttest: pk9Attest,
       eoaAddress,
       smartAccount,
       btcAddress,
@@ -149,6 +167,10 @@ export async function dataStructDecoder(response: IHaloDataStruct): Promise<IDat
     const eoaAddress = pknToAddressETH(pk8RawCompressed)
     const smartAccount = await computeGiftcardAddress(eoaAddress)
     const btcAddress = pknToAddressBTC(pk8RawCompressed)
+
+    if (typeof pk8Attest !== 'string') {
+      throw new Error('Missing pk8Attest.')
+    }
 
     return {
       pk2: uncompressPk(pk2RawCompressed),
