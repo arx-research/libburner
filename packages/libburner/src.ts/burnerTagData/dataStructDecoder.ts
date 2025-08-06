6b56f165387a0bc456aa8fdfb0f03e6a5aecc3cb
Robby Klein <hello@robbyk.com>
2025-08-06 10:30:19 -0700

    manually adjust formatting

--- packages/libburner/src.ts/burnerTagData/dataStructDecoder.ts
+++ packages/libburner/src.ts/burnerTagData/dataStructDecoder.ts
@@ -10,40 +10,40 @@ import {computeGiftcardAddress} from "../giftcard/index.js";
 
 
 export interface IHaloDataStruct {
-  isPartial: boolean;
-  data: Record<string, unknown>;
+  isPartial: boolean
+  data: Record<string, unknown>
 }
 
 export interface IHaloDataKeyFlags {
-  isExported: boolean;
-  isImported: boolean;
-  isPasswordProtected: boolean;
-  rawSignCommandNotUsed: boolean;
+  isExported: boolean
+  isImported: boolean
+  isPasswordProtected: boolean
+  rawSignCommandNotUsed: boolean
 }
 
 export type IDataStructDecoderResult = {
-  pk2: string;
-  pkN: string;
-  pkNAttest: string | undefined;
-  eoaAddress: Address;
-  smartAccount: Address;
-  btcAddress: string;
-  keyNumber: number;
-  color: string;
-  themeId: string;
-  graffiti: IGraffitiInfo | undefined;
-  theme: ITheme;
-  latch2Decoded: string | undefined;
-  pkNFlags: IHaloDataKeyFlags | undefined;
-};
+  pk2: string
+  pkN: string
+  pkNAttest: string | undefined
+  eoaAddress: Address
+  smartAccount: Address
+  btcAddress: string
+  keyNumber: number
+  color: string
+  themeId: string
+  graffiti: IGraffitiInfo | undefined
+  theme: ITheme
+  latch2Decoded: string | undefined
+  pkNFlags: IHaloDataKeyFlags | undefined
+}
 
 export function latchDecoder(latch: string | null): string | null {
   if (!latch) {
-    return null;
+    return null
   }
 
-  const decoded = hexDecode(latch);
-  return decoded.replace('.', '-');
+  const decoded = hexDecode(latch)
+  return decoded.replace('.', '-')
 }
 
 function isHaloDataKeyFlags(obj: any): obj is IHaloDataKeyFlags {
@@ -54,62 +54,62 @@ function isHaloDataKeyFlags(obj: any): obj is IHaloDataKeyFlags {
     typeof obj.isImported === 'boolean' &&
     typeof obj.isPasswordProtected === 'boolean' &&
     typeof obj.rawSignCommandNotUsed === 'boolean'
-  );
+  )
 }
 
 export async function dataStructDecoder(
   response: IHaloDataStruct
 ): Promise<IDataStructDecoderResult> {
-  const pk8RawCompressed = response.data['compressedPublicKey:8'];
-  const pk9RawCompressed = response.data['compressedPublicKey:9'];
-  const pk2RawCompressed = response.data['compressedPublicKey:2'];
-  const pk8Attest = response.data['publicKeyAttest:8'];
-  const pk9Attest = response.data['publicKeyAttest:9'];
-  const graffitiRaw = response.data['graffiti:1'];
-  const latch2 = response.data['latchValue:2'];
-  const pk8Flags = response.data['keySlotFlags:8'];
-  const pk9Flags = response.data['keySlotFlags:9'];
-  let theme = defaultTheme;
+  const pk8RawCompressed = response.data['compressedPublicKey:8']
+  const pk9RawCompressed = response.data['compressedPublicKey:9']
+  const pk2RawCompressed = response.data['compressedPublicKey:2']
+  const pk8Attest = response.data['publicKeyAttest:8']
+  const pk9Attest = response.data['publicKeyAttest:9']
+  const graffitiRaw = response.data['graffiti:1']
+  const latch2 = response.data['latchValue:2']
+  const pk8Flags = response.data['keySlotFlags:8']
+  const pk9Flags = response.data['keySlotFlags:9']
+  let theme = defaultTheme
 
   // Make sure we have everything we need
   if (typeof pk2RawCompressed !== 'string') {
-    throw new Error('Missing required pk2 parameters.');
+    throw new Error('Missing required pk2 parameters.')
   }
 
   if (typeof pk9RawCompressed === 'string') {
     // pk9 tag case
     if (typeof pk9Attest !== 'string') {
-      throw new Error('Missing required pk9 parameters.');
+      throw new Error('Missing required pk9 parameters.')
     }
   } else if (
     typeof pk8RawCompressed !== 'string' ||
     typeof pk8Attest !== 'string'
   ) {
     // pk8 tag case
-    throw new Error('Missing required pk8 parameters.');
+    throw new Error('Missing required pk8 parameters.')
   }
 
-  let _latchDecoded = null;
-  let latch2Decoded = undefined;
+  let _latchDecoded = null
+  let latch2Decoded = undefined
 
   // We might have color data
   if (typeof latch2 === 'string') {
-    _latchDecoded = latchDecoder(latch2);
+    _latchDecoded = latchDecoder(latch2)
 
     if (_latchDecoded !== null) {
-      latch2Decoded = _latchDecoded;
-      theme = findTheme(_latchDecoded);
+      latch2Decoded = _latchDecoded
+      theme = findTheme(_latchDecoded)
     }
   }
 
   // If there's a pk9 but not graffiti they aborted early, finish setup
   if (graffitiRaw == null && typeof pk9RawCompressed === 'string') {
-    const eoaAddress = pknToAddressETH(pk9RawCompressed);
-    const smartAccount = await computeGiftcardAddress(eoaAddress);
-    const btcAddress = pknToAddressBTC(pk9RawCompressed);
+    const eoaAddress = pknToAddressETH(pk9RawCompressed)
+    const smartAccount = await computeGiftcardAddress(eoaAddress)
+    const btcAddress = pknToAddressBTC(pk9RawCompressed)
 
     if (typeof pk9Attest !== 'string') {
-      throw new Error('Missing pk9Attest.');
+      throw new Error('Missing pk9Attest.')
     }
 
     return {
@@ -126,17 +126,17 @@ export async function dataStructDecoder(
       theme,
       latch2Decoded,
       pkNFlags: isHaloDataKeyFlags(pk9Flags) ? pk9Flags : undefined
-    };
+    }
   }
 
   // If its a new card save pk8 and go to step 1
   if (graffitiRaw === null && typeof pk8RawCompressed === 'string') {
-    const eoaAddress = pknToAddressETH(pk8RawCompressed);
-    const smartAccount = await computeGiftcardAddress(eoaAddress);
-    const btcAddress = pknToAddressBTC(pk8RawCompressed);
+    const eoaAddress = pknToAddressETH(pk8RawCompressed)
+    const smartAccount = await computeGiftcardAddress(eoaAddress)
+    const btcAddress = pknToAddressBTC(pk8RawCompressed)
 
     if (typeof pk8Attest !== 'string') {
-      throw new Error('Missing pk8Attest.');
+      throw new Error('Missing pk8Attest.')
     }
 
     return {
@@ -153,22 +153,22 @@ export async function dataStructDecoder(
       theme,
       latch2Decoded,
       pkNFlags: isHaloDataKeyFlags(pk8Flags) ? pk8Flags : undefined
-    };
+    }
   }
 
   // If it's a setup pk9 card go to dashboard
   // We wont have the pkNAttest in this scenario and
   // Will have to retrieve it later if required
   if (typeof pk9RawCompressed === 'string' && typeof graffitiRaw === 'string') {
-    const graffiti = graffitiDecoder(graffitiRaw);
-    const theme = findTheme(graffiti?.themeId);
+    const graffiti = graffitiDecoder(graffitiRaw)
+    const theme = findTheme(graffiti?.themeId)
 
-    const eoaAddress = pknToAddressETH(pk9RawCompressed);
-    const smartAccount = await computeGiftcardAddress(eoaAddress);
-    const btcAddress = pknToAddressBTC(pk9RawCompressed);
+    const eoaAddress = pknToAddressETH(pk9RawCompressed)
+    const smartAccount = await computeGiftcardAddress(eoaAddress)
+    const btcAddress = pknToAddressBTC(pk9RawCompressed)
 
     if (typeof pk9Attest !== 'string') {
-      throw new Error('Missing pk9Attest.');
+      throw new Error('Missing pk9Attest.')
     }
 
     return {
@@ -185,20 +185,20 @@ export async function dataStructDecoder(
       theme,
       latch2Decoded,
       pkNFlags: isHaloDataKeyFlags(pk9Flags) ? pk9Flags : undefined
-    };
+    }
   }
 
   // If it's a normal setup pk8 card
   if (typeof pk8RawCompressed === 'string' && typeof graffitiRaw === 'string') {
-    const graffiti = graffitiDecoder(graffitiRaw);
-    const theme = findTheme(graffiti?.themeId);
+    const graffiti = graffitiDecoder(graffitiRaw)
+    const theme = findTheme(graffiti?.themeId)
 
-    const eoaAddress = pknToAddressETH(pk8RawCompressed);
-    const smartAccount = await computeGiftcardAddress(eoaAddress);
-    const btcAddress = pknToAddressBTC(pk8RawCompressed);
+    const eoaAddress = pknToAddressETH(pk8RawCompressed)
+    const smartAccount = await computeGiftcardAddress(eoaAddress)
+    const btcAddress = pknToAddressBTC(pk8RawCompressed)
 
     if (typeof pk8Attest !== 'string') {
-      throw new Error('Missing pk8Attest.');
+      throw new Error('Missing pk8Attest.')
     }
 
     return {
@@ -215,8 +215,8 @@ export async function dataStructDecoder(
       theme,
       latch2Decoded,
       pkNFlags: isHaloDataKeyFlags(pk8Flags) ? pk8Flags : undefined
-    };
+    }
   }
 
-  throw new Error('Failed to parse data struct.');
+  throw new Error('Failed to parse data struct.')
 }
