46a9842a3182dab342eb29d1afbf61a48d9b8d61
Michał Leszczyński <ml@icedev.pl>
2026-03-06 22:25:56 +0100

    dataStructDecoder: add allowSkipAttest (#14)

--- packages/libburner/src.ts/burnerTagData/dataStructDecoder.ts
+++ packages/libburner/src.ts/burnerTagData/dataStructDecoder.ts
@@ -62,8 +62,13 @@ function isHaloDataKeyFlags(obj: any): obj is IHaloDataKeyFlags {
   )
 }
 
+interface IDataStructDecoderOptions {
+  allowSkipAttest?: boolean
+}
+
 export async function dataStructDecoder(
-  response: IHaloDataStruct
+  response: IHaloDataStruct,
+  options?: IDataStructDecoderOptions,
 ): Promise<IDataStructDecoderResult> {
   const pk8RawCompressed = response.data['compressedPublicKey:8']
   const pk9RawCompressed = response.data['compressedPublicKey:9']
@@ -76,6 +81,8 @@ export async function dataStructDecoder(
   const pk9Flags = response.data['keySlotFlags:9']
   let theme = defaultTheme
 
+  const allowSkipAttest = options?.allowSkipAttest
+
   // Make sure we have everything we need
   if (typeof pk2RawCompressed !== 'string') {
     throw new Error('Missing required pk2 parameters.')
@@ -83,12 +90,12 @@ export async function dataStructDecoder(
 
   if (typeof pk9RawCompressed === 'string') {
     // pk9 tag case
-    if (typeof pk9Attest !== 'string') {
+    if (!allowSkipAttest && typeof pk9Attest !== 'string') {
       throw new Error('Missing required pk9 parameters.')
     }
   } else if (
     typeof pk8RawCompressed !== 'string' ||
-    typeof pk8Attest !== 'string'
+    (!allowSkipAttest && typeof pk8Attest !== 'string')
   ) {
     // pk8 tag case
     throw new Error('Missing required pk8 parameters.')
@@ -113,15 +120,18 @@ export async function dataStructDecoder(
     const smartAccount = await computeGiftcardAddress(eoaAddress)
     const btcAddress = pknToAddressBTC(pk9RawCompressed)
     const solAddresses = pknToSOLAddresses(pk9RawCompressed)
+    const usePk9Attest = typeof pk9Attest === 'string'
+      ? pk9Attest
+      : undefined
 
-    if (typeof pk9Attest !== 'string') {
+    if (!allowSkipAttest && !usePk9Attest) {
       throw new Error('Missing pk9Attest.')
     }
 
     return {
       pk2: uncompressPk(pk2RawCompressed),
       pkN: uncompressPk(pk9RawCompressed),
-      pkNAttest: pk9Attest,
+      pkNAttest: usePk9Attest,
       eoaAddress,
       smartAccount,
       btcAddress,
@@ -142,15 +152,18 @@ export async function dataStructDecoder(
     const smartAccount = await computeGiftcardAddress(eoaAddress)
     const btcAddress = pknToAddressBTC(pk8RawCompressed)
     const solAddresses = pknToSOLAddresses(pk8RawCompressed)
+    const usePk8Attest = typeof pk8Attest === 'string'
+      ? pk8Attest
+      : undefined
 
-    if (typeof pk8Attest !== 'string') {
+    if (!allowSkipAttest && !usePk8Attest) {
       throw new Error('Missing pk8Attest.')
     }
 
     return {
       pk2: uncompressPk(pk2RawCompressed),
       pkN: uncompressPk(pk8RawCompressed),
-      pkNAttest: pk8Attest,
+      pkNAttest: usePk8Attest,
       eoaAddress,
       smartAccount,
       btcAddress,
@@ -176,15 +189,18 @@ export async function dataStructDecoder(
     const smartAccount = await computeGiftcardAddress(eoaAddress)
     const btcAddress = pknToAddressBTC(pk9RawCompressed)
     const solAddresses = pknToSOLAddresses(pk9RawCompressed)
+    const usePk9Attest = typeof pk9Attest === 'string'
+      ? pk9Attest
+      : undefined
 
-    if (typeof pk9Attest !== 'string') {
+    if (!allowSkipAttest && !usePk9Attest) {
       throw new Error('Missing pk9Attest.')
     }
 
     return {
       pk2: uncompressPk(pk2RawCompressed),
       pkN: uncompressPk(pk9RawCompressed),
-      pkNAttest: pk9Attest,
+      pkNAttest: usePk9Attest,
       eoaAddress,
       smartAccount,
       btcAddress,
@@ -208,15 +224,18 @@ export async function dataStructDecoder(
     const smartAccount = await computeGiftcardAddress(eoaAddress)
     const btcAddress = pknToAddressBTC(pk8RawCompressed)
     const solAddresses = pknToSOLAddresses(pk8RawCompressed)
+    const usePk8Attest = typeof pk8Attest === 'string'
+      ? pk8Attest
+      : undefined
 
-    if (typeof pk8Attest !== 'string') {
+    if (!allowSkipAttest && typeof pk8Attest !== 'string') {
       throw new Error('Missing pk8Attest.')
     }
 
     return {
       pk2: uncompressPk(pk2RawCompressed),
       pkN: uncompressPk(pk8RawCompressed),
-      pkNAttest: pk8Attest,
+      pkNAttest: usePk8Attest,
       eoaAddress,
       smartAccount,
       btcAddress,
